7190256cc56a62c5f2cbc896e914ef127c63575a
Benjie Gillam <benjie@jemjie.com>
2013-06-26 23:00:59 +0100

    Add tmux config.

--- install.sh
+++ install.sh
@@ -4,6 +4,7 @@ git clone https://github.com/gmarik/vundle.git ~/.vim/bundle/vundle
 ln -s "`pwd`"/vimrc ../.vimrc
 ln -s "`pwd`"/zshrc ../.zshrc
 ln -s "`pwd`"/profile_common ../.profile_common
+ln -s "`pwd`"/tmux.conf ../.tmux.conf
 mkdir -p ~/.vim/plugin/after/
 ln -s vim/plugin/after/* ~/.vim/plugin/after/
 vim -c 'BundleInstall!' -c "q" -c "q"
