1407319e374fd09a8a541c6783f21511618b00d0
Benjie Gillam <benjie@jemjie.com>
2013-11-05 13:37:32 +0000

    Add .gitconfig

--- install.sh
+++ install.sh
@@ -5,6 +5,7 @@ ln -s "`pwd`"/vimrc ../.vimrc
 ln -s "`pwd`"/zshrc ../.zshrc
 ln -s "`pwd`"/profile_common ../.profile_common
 ln -s "`pwd`"/tmux.conf ../.tmux.conf
+ln -s "`pwd`"/gitconfig ../.gitconfig
 mkdir -p ~/.vim/plugin/after/
 ln -s vim/plugin/after/* ~/.vim/plugin/after/
 vim +BundleInstall +qall
