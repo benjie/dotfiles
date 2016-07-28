db22091f21bd708d8b65b40e56aec7ab5ba98180
Benjie Gillam <benjie@jemjie.com>
2016-07-28 14:38:06 +0100

    Better tmux script

--- starttmux.sh
+++ starttmux.sh
@@ -1,10 +1,9 @@
 TMUX="/usr/local/bin/tmux"
-$TMUX ls
-if [ "$?" == "0" ]; then
-  echo "Connecting to old tmux"
-else
+if [ "$(echo $($TMUX ls | wc -l))" == "0" ]; then
   echo "Creating new tmux server"
   ~/.tmux-general.sh
   ~/Documents/timecounts-frontend/tmux.sh
+else
+  echo "Connecting to old tmux"
 fi
 $TMUX attach-session -t General
