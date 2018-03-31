f0d5ca35e255184bae84ca754d5227739e0bd487
Benjie Gillam <benjie@jemjie.com>
2018-03-31 23:27:12 +0100

    Python 3

--- starttmux.sh
+++ starttmux.sh
@@ -1,8 +1,7 @@
 TMUX="/usr/local/bin/tmux"
 if [ "$(echo $($TMUX ls | wc -l))" == "0" ]; then
   echo "Creating new tmux server"
-  #~/.tmux-general.sh
-  ~/Documents/HookHaven/.tmux.sh
+  ~/.tmux-general.sh
 else
   echo "Connecting to old tmux"
 fi
