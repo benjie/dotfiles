89315814ab933a7330f7b12f8b5f371cd6641ab6
Benjie Gillam <benjie@jemjie.com>
2024-10-11 16:14:36 +0100

    Show hidden files

--- init.lua
+++ init.lua
@@ -88,6 +88,15 @@ require('packer').startup(function(use)
       {'nvim-lua/plenary.nvim' }
     },
     config = function()
+      require('telescope').setup{
+        pickers = {
+          live_grep = {
+            additional_args = function(opts)
+              return {"--hidden"}
+            end
+          },
+        }
+      }
       require("telescope").load_extension("live_grep_args")
     end
   }
@@ -448,8 +457,8 @@ vim.keymap.set('n', '<leader>/', function()
   })
 end, { desc = '[/] Fuzzily search in current buffer]' })
 
-vim.keymap.set('n', '<C-p>', require('telescope.builtin').find_files, { desc = '' })
-vim.keymap.set('n', '<leader>sf', require('telescope.builtin').find_files, { desc = '[S]earch [F]iles' })
+vim.keymap.set('n', '<C-p>', function() require('telescope.builtin').find_files({hidden = true}) end, { desc = '' })
+vim.keymap.set('n', '<leader>sf', function() require('telescope.builtin').find_files({hidden = true}) end, { desc = '[S]earch [F]iles' })
 vim.keymap.set('n', '<leader>sh', require('telescope.builtin').help_tags, { desc = '[S]earch [H]elp' })
 vim.keymap.set('n', '<leader>sw', require('telescope.builtin').grep_string, { desc = '[S]earch current [W]ord' })
 vim.keymap.set('n', '<leader>sg', require('telescope.builtin').live_grep, { desc = '[S]earch by [G]rep' })
