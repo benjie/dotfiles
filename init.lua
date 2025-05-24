17c7b5f273a32c4d99b20046ae175f05c820da9e
Benjie Gillam <benjie@jemjie.com>
2025-05-24 10:54:08 +0100

    Overhaul LSP config

--- init.lua
+++ init.lua
@@ -612,87 +612,8 @@ vim.keymap.set('n', ']d', vim.diagnostic.goto_next)
 vim.keymap.set('n', '<leader>e', vim.diagnostic.open_float)
 vim.keymap.set('n', '<leader>q', vim.diagnostic.setloclist)
 
--- LSP settings.
---  This function gets run when an LSP connects to a particular buffer.
-local on_attach = function(_, bufnr)
-  -- NOTE: Remember that lua is a real programming language, and as such it is possible
-  -- to define small helper and utility functions so you don't have to repeat yourself
-  -- many times.
-  --
-  -- In this case, we create a function that lets us more easily define mappings specific
-  -- for LSP related items. It sets the mode, buffer and description for us each time.
-  local nmap = function(keys, func, desc)
-    if desc then
-      desc = 'LSP: ' .. desc
-    end
-
-    vim.keymap.set('n', keys, func, { buffer = bufnr, desc = desc })
-  end
-
-  nmap('<leader>rn', vim.lsp.buf.rename, '[R]e[n]ame')
-  -- Use this to accept suggestions from LSP/TypeScript/etc
-  nmap('<leader>ca', vim.lsp.buf.code_action, '[C]ode [A]ction')
-
-  nmap('gd', vim.lsp.buf.definition, '[G]oto [D]efinition')
-  nmap('gr', require('telescope.builtin').lsp_references, '[G]oto [R]eferences')
-  nmap('gI', vim.lsp.buf.implementation, '[G]oto [I]mplementation')
-  nmap('<leader>D', vim.lsp.buf.type_definition, 'Type [D]efinition')
-  nmap('<leader>ds', require('telescope.builtin').lsp_document_symbols, '[D]ocument [S]ymbols')
-  nmap('<leader>ws', require('telescope.builtin').lsp_dynamic_workspace_symbols, '[W]orkspace [S]ymbols')
-
-  -- See `:help K` for why this keymap
-  nmap('K', vim.lsp.buf.hover, 'Hover Documentation')
-  nmap('<C-d>', vim.lsp.buf.signature_help, 'Signature [D]ocumentation')
-
-  -- Lesser used LSP functionality
-  nmap('gD', vim.lsp.buf.declaration, '[G]oto [D]eclaration')
-  nmap('<leader>wa', vim.lsp.buf.add_workspace_folder, '[W]orkspace [A]dd Folder')
-  nmap('<leader>wr', vim.lsp.buf.remove_workspace_folder, '[W]orkspace [R]emove Folder')
-  nmap('<leader>wl', function()
-    print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
-  end, '[W]orkspace [L]ist Folders')
-
-  -- Create a command `:Format` local to the LSP buffer
-  vim.api.nvim_buf_create_user_command(bufnr, 'Format', function(_)
-    vim.lsp.buf.format()
-  end, { desc = 'Format current buffer with LSP' })
-end
-
--- Enable the following language servers
---  Feel free to add/remove any LSPs that you want here. They will automatically be installed.
---
---  Add any additional override configuration in the following tables. They will be passed to
---  the `settings` field of the server config. You must look up that documentation yourself.
-local servers = {
-  -- clangd = {},
-  -- gopls = {},
-  -- pyright = {},
-  -- rust_analyzer = {},
-  ts_ls = {},
-
-  lua_ls = {
-    Lua = {
-      workspace = {
-        checkThirdParty = false, -- Avoid unnecessary third-party plugin warnings
-        library = {
-          vim.env.VIMRUNTIME, -- Neovim runtime files
-          "${3rd}/luv/library",
-          unpack(vim.api.nvim_get_runtime_file("", true)),
-        },
-      },
-      telemetry = { enable = false },
-    },
-  },
-
-  graphql = {}
-}
-
 -- Setup neovim lua configuration
 require('neodev').setup()
---
--- nvim-cmp supports additional completion capabilities, so broadcast that to servers
-local capabilities = vim.lsp.protocol.make_client_capabilities()
--- capabilities = require('cmp_nvim_lsp').default_capabilities(capabilities)
 
 -- Turn on lsp status information
 require('fidget').setup()
@@ -775,54 +696,95 @@ cmp.setup {
   }
 }
 
--- Setup mason so it can manage external tooling
-require('mason').setup()
-
--- Ensure the servers above are installed
-local mason_lspconfig = require 'mason-lspconfig'
-
 -- vim.lsp.set_log_level("debug")
 
-mason_lspconfig.setup {
-  ensure_installed = vim.tbl_keys(servers),
-}
+-- nvim-cmp supports additional completion capabilities, so broadcast that to servers
+local capabilities = vim.lsp.protocol.make_client_capabilities()
+-- capabilities = require('cmp_nvim_lsp').default_capabilities(capabilities)
+-- Apply to all language servers
+vim.lsp.config("*", { capabilities = capabilities })
 
-mason_lspconfig.setup_handlers {
-  function(server_name)
-    -- vim.print(server_name)
-    if (server_name == 'graphql') then
-      -- vim.print(server_name)
-      local lspconfig = require('lspconfig')
-      lspconfig.graphql.setup {
-        capabilities = capabilities,
-        -- on_attach = on_attach,
-        on_attach = function (client, bufnr)
-          print("GraphQL LSP attached")
-        end,
-        -- root_dir = lspconfig.util.root_pattern(".graphqlconfig", ".graphqlrc", "package.json"),
-        flags = {
-          debounce_text_changes = 150,
+vim.lsp.config("graphql", {
+  -- root_dir = lspconfig.util.root_pattern(".graphqlconfig", ".graphqlrc", "package.json"),
+  flags = {
+    debounce_text_changes = 150,
+  },
+  -- filetypes = { "graphql" },
+  --settings = {
+  --  graphql = {
+  --    schemaPath = './simple.graphqls',
+  --    documents = '*.graphql'
+  --  }
+  --}
+})
+
+vim.lsp.config("lua_ls", {
+  settings = {
+    Lua = {
+      workspace = {
+        checkThirdParty = false, -- Avoid unnecessary third-party plugin warnings
+        library = {
+          vim.env.VIMRUNTIME, -- Neovim runtime files
+          "${3rd}/luv/library",
+          unpack(vim.api.nvim_get_runtime_file("", true)),
         },
-        -- filetypes = { "graphql" },
-        --settings = {
-        --  graphql = {
-        --    schemaPath = './simple.graphqls',
-        --    documents = '*.graphql'
-        --  }
-        --}
-      }
-      -- vim.print('GraphQL setup called')
-    else
-      require('lspconfig')[server_name].setup {
-        capabilities = capabilities,
-        on_attach = on_attach,
-        settings = servers[server_name],
-      }
+      },
+      telemetry = { enable = false },
+    },
+  },
+})
+
+vim.api.nvim_create_autocmd('LspAttach', {
+  callback = function(ev)
+    local client = vim.lsp.get_client_by_id(ev.data.client_id)
+    if not client then return end
+    local bufnr = ev.buf
+
+    local nmap = function(keys, func, desc)
+      if desc then desc = 'LSP: ' .. desc end
+      vim.keymap.set('n', keys, func, { buffer = bufnr, desc = desc })
     end
-  end,
+
+    nmap('<leader>rn', vim.lsp.buf.rename, '[R]e[n]ame')
+    -- Use this to accept suggestions from LSP/TypeScript/etc
+    nmap('<leader>ca', vim.lsp.buf.code_action, '[C]ode [A]ction')
+
+    nmap('gd', vim.lsp.buf.definition, '[G]oto [D]efinition')
+    nmap('gr', require('telescope.builtin').lsp_references, '[G]oto [R]eferences')
+    nmap('gI', vim.lsp.buf.implementation, '[G]oto [I]mplementation')
+    nmap('<leader>D', vim.lsp.buf.type_definition, 'Type [D]efinition')
+    nmap('<leader>ds', require('telescope.builtin').lsp_document_symbols, '[D]ocument [S]ymbols')
+    nmap('<leader>ws', require('telescope.builtin').lsp_dynamic_workspace_symbols, '[W]orkspace [S]ymbols')
+
+    -- See `:help K` for why this keymap
+    nmap('K', vim.lsp.buf.hover, 'Hover Documentation')
+    nmap('<C-d>', vim.lsp.buf.signature_help, 'Signature [D]ocumentation')
+
+    -- Lesser used LSP functionality
+    nmap('gD', vim.lsp.buf.declaration, '[G]oto [D]eclaration')
+    nmap('<leader>wa', vim.lsp.buf.add_workspace_folder, '[W]orkspace [A]dd Folder')
+    nmap('<leader>wr', vim.lsp.buf.remove_workspace_folder, '[W]orkspace [R]emove Folder')
+    nmap('<leader>wl', function()
+      print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
+    end, '[W]orkspace [L]ist Folders')
+
+    -- Create a command `:Format` local to the LSP buffer
+    vim.api.nvim_buf_create_user_command(bufnr, 'Format', function(_)
+      vim.lsp.buf.format()
+    end, { desc = 'Format current buffer with LSP' })
+
+    if client.name == "graphql" then
+      print("GraphQL LSP attached")
+    end
+  end
+})
+
+-- Setup mason so it can manage external tooling
+require('mason').setup()
+require('mason-lspconfig').setup {
+  ensure_installed = {'ts_ls', 'lua_ls', 'graphql'},
+  automatic_enable = true
 }
-
-
 
 vim.api.nvim_set_hl(0, "CmpItemKindCopilot", {fg ="#6CC644"})
 
